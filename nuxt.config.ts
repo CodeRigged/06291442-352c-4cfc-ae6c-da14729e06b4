a6c52733ce339e769e78a739f58e6eb04e8d0714
CodeRigged <bingobookdeath@yahoo.de>
2024-03-08 22:15:48 +0100

    removes pinia since it wasnt used

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: {
+    global: true,
+    dirs: ['~/components'],
+  },
   devtools: { enabled: true },
   build: {
     transpile: ['vuetify'],
   },
+
   modules: [
     '@nuxtjs/eslint-module',
     '@vueuse/nuxt',
-    '@pinia/nuxt',
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
